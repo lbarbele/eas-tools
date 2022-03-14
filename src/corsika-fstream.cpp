74fd90f23fa4a8c08d61eaf7359ca1126cf57936
Luan Arbeletche <luan.arbeletche@gmail.com>
2022-03-14 14:59:34 -0300

    implement corsika::fstream

---
+++ src/corsika-fstream.cpp
@@ -0,0 +1,91 @@
+#include "corsika-fstream.h"
+
+namespace corsika {
+
+  // constructors: construct the base with the equivalent constructor and
+  // imbue a locale containing the word_codevt conversion facet
+  // if opening a file, always set binary mode
+  fstream::fstream()
+  : std::basic_fstream<word_t>()
+  {
+    imbue(std::locale(std::locale(), new word_codecvt));
+  }
+
+  fstream::fstream(
+    const char* fname,
+    ios_base::openmode mode
+  ) :
+    std::basic_fstream<word_t>(fname, mode|ios_base::binary)
+  {
+    imbue(std::locale(std::locale(), new word_codecvt));
+  }
+
+  fstream::fstream(
+    const std::string& fname,
+    ios_base::openmode mode
+  ) :
+    std::basic_fstream<word_t>(fname, mode|ios_base::binary)
+  {
+    imbue(std::locale(std::locale(), new word_codecvt));
+  }
+
+  fstream::fstream(
+    const std::filesystem::path& fname,
+    ios_base::openmode mode
+  ) :
+    std::basic_fstream<word_t>(fname, mode|ios_base::binary)
+  {
+    imbue(std::locale(std::locale(), new word_codecvt));
+  }
+
+  // fstream::open: call base's open, but always adding ios_base::binary
+  // to the open mode
+  void
+  fstream::open(
+    const char* fname,
+    ios_base::openmode mode
+  )
+  {
+    std::basic_fstream<word_t>::open(fname, mode|ios_base::binary);
+  }
+
+  void
+  fstream::open(
+    const std::string& fname,
+    ios_base::openmode mode
+  )
+  {
+    std::basic_fstream<word_t>::open(fname, mode|ios_base::binary);
+  }
+
+  void
+  fstream::open(
+    const std::filesystem::path& fname,
+    ios_base::openmode mode
+  )
+  {
+    std::basic_fstream<word_t>::open(fname, mode|ios_base::binary);
+  }
+
+  // insertion/extraction operators
+  std::basic_istream<word_t>&
+  operator>>(
+    std::basic_istream<word_t>& stream,
+    word_t& word
+  )
+  {
+    stream.get(word);
+    return stream;
+  }
+
+  std::basic_ostream<word_t>&
+  operator<<(
+    std::basic_ostream<word_t>& stream,
+    word_t& word
+  )
+  {
+    stream.put(word);
+    return stream;
+  }
+
+} // namespace corsika
---
+++ src/corsika-fstream.h
@@ -0,0 +1,35 @@
+#ifndef _corsika_fstream_h
+#define _corsika_fstream_h
+
+#include <fstream>
+#include <filesystem>
+#include <locale>
+#include <string>
+
+#include "corsika-word_t.h"
+
+namespace corsika {
+
+  class fstream : public std::basic_fstream<word_t> {
+  private:
+  public:
+    fstream();
+    fstream(const char* fname, ios_base::openmode mode = ios_base::in);
+    fstream(const std::string& fname, ios_base::openmode mode = ios_base::in);
+    fstream(const std::filesystem::path& fname, ios_base::openmode mode = ios_base::in);
+
+    void open(const char* fname, ios_base::openmode mode = ios_base::in);
+    void open(const std::string& fname, ios_base::openmode mode = ios_base::in);
+    void open(const std::filesystem::path& fname, ios_base::openmode mode = ios_base::in);
+  };
+
+  // declare operators
+  std::basic_istream<word_t>&
+  operator>>(std::basic_istream<word_t>& stream, word_t& word);
+
+  std::basic_ostream<word_t>&
+  operator<<(std::basic_ostream<word_t>& stream, word_t& word);
+
+} // namespace corsika
+
+#endif
