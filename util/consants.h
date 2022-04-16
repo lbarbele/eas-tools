e1ecf9fc13e50a5d727a823d74c4b1d958582d3e
Luan Arbeletche <luan.arbeletche@gmail.com>
2022-04-16 11:15:37 -0300

    add header with physical constants

---
+++ util/consants.h
@@ -0,0 +1,15 @@
+#ifndef _util_constants_h
+#define _util_constants_h
+
+namespace util::constants {
+  // constants in SI
+  constexpr double c = 299792458.;
+  constexpr double pi = 3.141592653589793238462643;
+  constexpr double earth_radius = 6371315.;
+
+  // other units
+  constexpr double proton_mass_gev = 0.93827208816;
+
+} // namespace util::constants
+
+#endif
