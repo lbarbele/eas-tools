d323aeb4866f0f80b44b791ee5d1d46c25b7954f
Luan Arbeletche <luan.arbeletche@gmail.com>
2022-06-13 17:58:27 -0300

    read file through a TChain

--- include/conex/file.h
+++ include/conex/file.h
@@ -2,39 +2,61 @@
 #define _conex_file_h
 
 #include <string>
+#include <vector>
 #include <memory>
 #include <cstddef>
+#include <istream>
+#include <iterator>
 
-#include <TFile.h>
-#include <TTree.h>
+#include <TChain.h>
 
 #include <conex/header.h>
 #include <conex/shower.h>
 
 namespace conex {
 
-  class file : private TFile {
+  class file {
   public:
     class iterator;
   // TODO: add support to the leading interactions tree
-  // TODO: add a shower iterator, so that range-based for loops can be used
   private:
-
-    std::unique_ptr<TTree> m_header_tree;
-    std::unique_ptr<TTree> m_shower_tree;
-
-    header m_header;
+    std::unique_ptr<TChain> m_shower_tree;
     shower m_shower;
+    std::vector<std::string> m_file_names;
 
   public:
-    file(const std::string& fname);
+    // main constructor: receives a vector of file names
+    file(const std::vector<std::string>& fnames);
+
+    // constructor receiving a list of (convertible to) string arguments, each one pointing to a
+    // single file. delegates construction to the main constructor
+    template <
+      class... Args,
+      class T = std::enable_if_t< 
+        std::conjunction_v<std::is_convertible<Args, std::string>...> ,
+        std::string
+      > 
+    >
+    file(Args... args)
+    : file(std::vector<T>{args...})
+    {}
+
+    // constructor receiving an input stream. reads the stream contents as a sequence of strings
+    // into a vector and delegates to the main constructor
+    template <class CharT, class Traits = std::char_traits<CharT>>
+    file(std::basic_istream<CharT, Traits>& stream)
+    : file(std::vector<std::string>{std::istream_iterator<std::string>(stream), std::istream_iterator<std::string>()})
+    {}
 
     bool is_open() const;
-    const header& get_header() const;
+    header get_header() const;
+
+    unsigned int get_n_files() const;
+    int get_current_file_number() const;
+    std::string get_current_file_name() const;
+
     long long get_n_showers() const;
     const shower& get_shower(size_t pos);
-    const shower& get_shower() const;
-
     const shower& operator[](size_t pos);
 
     iterator begin();
--- src/conex/file.cpp
+++ src/conex/file.cpp
@@ -1,76 +1,35 @@
 #include <string>
 #include <iostream>
 
+#include <TFile.h>
+
 #include <conex/file.h>
 #include <conex/shower.h>
 
 namespace conex {
 
   file::file(
-    const std::string& fname
+    const std::vector<std::string>& fnames
   ) :
-    TFile(fname.c_str(), "read")
+    m_shower_tree(std::make_unique<TChain>("Shower")),
+    m_file_names(fnames)
   {
-    // check if file is open
-    if (!is_open()) {
-      std::cerr << "unable to open conex file " << fname << std::endl;
+    // do nothing if the list of file names is empty
+    if (fnames.empty()) {
       return;
     }
 
-    // get the trees and check
-    m_header_tree.reset(Get<TTree>("Header"));
-    m_shower_tree.reset(Get<TTree>("Shower"));
-
-    if (!m_header_tree || !m_shower_tree) {
-      m_header_tree.reset(nullptr);
-      m_shower_tree.reset(nullptr);
-      Close();
-      return;
+    // try to add the files to the TChain
+    for (const auto& file_path : fnames) {
+      if (m_shower_tree->Add(file_path.c_str(), 0) != 1) {
+        std::cerr << "failed to open file " + file_path << std::endl;
+        m_shower_tree.reset(nullptr);
+        m_file_names.clear();
+        return;
+      }
     }
 
-    // set addresses of the header tree
-    m_header_tree->SetBranchAddress("Seed1",&m_header.Seed1);
-    m_header_tree->SetBranchAddress("Particle",&m_header.Particle);
-    m_header_tree->SetBranchAddress("Alpha",&m_header.Alpha);
-    m_header_tree->SetBranchAddress("lgEmin",&m_header.lgEmin);
-    m_header_tree->SetBranchAddress("lgEmax",&m_header.lgEmax);
-    m_header_tree->SetBranchAddress("zMin",&m_header.zMin);
-    m_header_tree->SetBranchAddress("zMax",&m_header.zMax);
-    m_header_tree->SetBranchAddress("Version",&m_header.Version);
-    m_header_tree->SetBranchAddress("OutputVersion",&m_header.OutputVersion);
-    m_header_tree->SetBranchAddress("HEModel",&m_header.HEModel);
-    m_header_tree->SetBranchAddress("LEModel",&m_header.LEModel);
-    m_header_tree->SetBranchAddress("HiLowEgy",&m_header.HiLowEgy);
-    m_header_tree->SetBranchAddress("hadCut",&m_header.hadCut);
-    m_header_tree->SetBranchAddress("emCut",&m_header.emCut);
-    m_header_tree->SetBranchAddress("hadThr",&m_header.hadThr);
-    m_header_tree->SetBranchAddress("muThr",&m_header.muThr);
-    m_header_tree->SetBranchAddress("emThr",&m_header.emThr);
-    m_header_tree->SetBranchAddress("haCut",&m_header.haCut);
-    m_header_tree->SetBranchAddress("muCut",&m_header.muCut);
-    m_header_tree->SetBranchAddress("elCut",&m_header.elCut);
-    m_header_tree->SetBranchAddress("gaCut",&m_header.gaCut);
-
-    m_header_tree->SetBranchAddress("lambdaLgE",m_header.lambdaLgE.data());
-    m_header_tree->SetBranchAddress("lambdaProton",m_header.lambdaProton.data());
-    m_header_tree->SetBranchAddress("lambdaPion",m_header.lambdaPion.data());
-    m_header_tree->SetBranchAddress("lambdaHelium",m_header.lambdaHelium.data());
-    m_header_tree->SetBranchAddress("lambdaNitrogen",m_header.lambdaNitrogen.data());
-    m_header_tree->SetBranchAddress("lambdaIron",m_header.lambdaIron.data());
-
-    if (m_header_tree->GetNbranches() > 27) {
-      m_header_tree->SetBranchAddress("resamplingMode",&m_header.resamplingMode);
-      m_header_tree->SetBranchAddress("modThreshold",&m_header.modThreshold);
-      m_header_tree->SetBranchAddress("f19_cx",&m_header.f19_cx);
-      m_header_tree->SetBranchAddress("f19_meson",&m_header.f19_meson);
-      m_header_tree->SetBranchAddress("f19",&m_header.f19);
-
-      m_header.m_has_extensions = true;
-    } else {
-      m_header.m_has_extensions = false;
-    }
-
-    // set addresses of the shower tree
+    // set the branch addresses of the Shower tree
     m_shower_tree->SetBranchAddress("lgE",&m_shower.lgE);
     m_shower_tree->SetBranchAddress("zenith",&m_shower.zenith);
     m_shower_tree->SetBranchAddress("azimuth",&m_shower.azimuth);
@@ -105,23 +64,74 @@ namespace conex {
     m_shower_tree->SetBranchAddress("Hadrons",m_shower.Hadrons);
     m_shower_tree->SetBranchAddress("dMu",m_shower.dMu);
     m_shower_tree->SetBranchAddress("EGround",m_shower.EGround);
-
-    // actually read the header
-    m_header_tree->GetEntry(0);
   }
-
+  
   bool
   file::is_open()
   const
   {
-    return IsOpen() && !IsZombie() && !TestBit(kRecovered);
+    return bool(m_shower_tree);
   }
 
-  const header&
+  header
   file::get_header()
   const
   {
-    return m_header;
+    // get pointer to the current TFile
+    auto current_file = m_shower_tree->GetFile();
+
+    // get the header tree from the current file
+    auto header_tree = std::unique_ptr<TTree>(current_file->Get<TTree>("Header"));
+
+    // create a header object
+    header header_obj;
+
+    // connect the header object with the header tree
+    header_tree->SetBranchAddress("Seed1",&header_obj.Seed1);
+    header_tree->SetBranchAddress("Particle",&header_obj.Particle);
+    header_tree->SetBranchAddress("Alpha",&header_obj.Alpha);
+    header_tree->SetBranchAddress("lgEmin",&header_obj.lgEmin);
+    header_tree->SetBranchAddress("lgEmax",&header_obj.lgEmax);
+    header_tree->SetBranchAddress("zMin",&header_obj.zMin);
+    header_tree->SetBranchAddress("zMax",&header_obj.zMax);
+    header_tree->SetBranchAddress("Version",&header_obj.Version);
+    header_tree->SetBranchAddress("OutputVersion",&header_obj.OutputVersion);
+    header_tree->SetBranchAddress("HEModel",&header_obj.HEModel);
+    header_tree->SetBranchAddress("LEModel",&header_obj.LEModel);
+    header_tree->SetBranchAddress("HiLowEgy",&header_obj.HiLowEgy);
+    header_tree->SetBranchAddress("hadCut",&header_obj.hadCut);
+    header_tree->SetBranchAddress("emCut",&header_obj.emCut);
+    header_tree->SetBranchAddress("hadThr",&header_obj.hadThr);
+    header_tree->SetBranchAddress("muThr",&header_obj.muThr);
+    header_tree->SetBranchAddress("emThr",&header_obj.emThr);
+    header_tree->SetBranchAddress("haCut",&header_obj.haCut);
+    header_tree->SetBranchAddress("muCut",&header_obj.muCut);
+    header_tree->SetBranchAddress("elCut",&header_obj.elCut);
+    header_tree->SetBranchAddress("gaCut",&header_obj.gaCut);
+
+    header_tree->SetBranchAddress("lambdaLgE",header_obj.lambdaLgE.data());
+    header_tree->SetBranchAddress("lambdaProton",header_obj.lambdaProton.data());
+    header_tree->SetBranchAddress("lambdaPion",header_obj.lambdaPion.data());
+    header_tree->SetBranchAddress("lambdaHelium",header_obj.lambdaHelium.data());
+    header_tree->SetBranchAddress("lambdaNitrogen",header_obj.lambdaNitrogen.data());
+    header_tree->SetBranchAddress("lambdaIron",header_obj.lambdaIron.data());
+
+    if (header_tree->GetNbranches() > 27) {
+      header_tree->SetBranchAddress("resamplingMode",&header_obj.resamplingMode);
+      header_tree->SetBranchAddress("modThreshold",&header_obj.modThreshold);
+      header_tree->SetBranchAddress("f19_cx",&header_obj.f19_cx);
+      header_tree->SetBranchAddress("f19_meson",&header_obj.f19_meson);
+      header_tree->SetBranchAddress("f19",&header_obj.f19);
+
+      header_obj.m_has_extensions = true;
+    } else {
+      header_obj.m_has_extensions = false;
+    }
+
+    // actually read the header
+    header_tree->GetEntry(0);
+
+    return header_obj;
   }
 
   long long
@@ -131,19 +141,33 @@ namespace conex {
     return m_shower_tree->GetEntries();
   }
 
+  unsigned int
+  file::get_n_files()
+  const
+  {
+    return m_file_names.size();
+  }
+
+  int
+  file::get_current_file_number()
+  const
+  {
+    return is_open()? m_shower_tree->GetTreeNumber(): -1;
+  }
+
+  std::string
+  file::get_current_file_name()
+  const
+  {
+    return is_open()? m_shower_tree->GetFile()->GetName() : "";
+  }
+
   const shower&
   file::get_shower(
     size_t pos
   )
   {
     m_shower_tree->GetEntry(pos);
-    return m_shower;
-  }
-
-  const shower&
-  file::get_shower()
-  const
-  {
     return m_shower;
   }
 
