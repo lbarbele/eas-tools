3a2aefc493a68d3e25a9c20cbab3cb0c4a28f1a1
Luan Arbeletche <luan.arbeletche@gmail.com>
2022-06-23 19:16:58 -0300

    fix type traits

--- include/util/type_traits.h
+++ include/util/type_traits.h
@@ -3,20 +3,21 @@
 
 #include <type_traits>
 
-#include <util/matrix.h>
-#include <util/vector.h>
-
 namespace util {
 
-  // * type T is derived from matrix_base
+  // * base classes
+  class matrix_base;
+  class vector_base;
+
+  // * enable if derived from matrix_base
   template <class T, class U = void>
   using enable_if_matrix_t = std::enable_if_t<std::is_base_of_v<matrix_base, T>, U>;
 
-  // * type T is derived from vector_base
+  // * enable if derived from vector_base
   template <class T, class U = void>
   using enable_if_vector_t = std::enable_if_t<std::is_base_of_v<vector_base, T>, U>;
 
-  // * type T is neither vector nor matrix
+  // * enable if not derived from vector nor from matrix
   template <class T, class U = void>
   using enable_if_scalar_t = std::enable_if_t<
     !std::is_base_of_v<matrix_base, T> && !std::is_base_of_v<vector_base, T>
