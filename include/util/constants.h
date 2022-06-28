f4af69459e96127d23c21611afb120a7744ebb66
Luan Arbeletche <luan.arbeletche@gmail.com>
2022-06-28 17:24:09 -0300

    annotate units

--- include/util/constants.h
+++ include/util/constants.h
@@ -3,9 +3,9 @@
 
 namespace util::constants {
   // constants in SI
-  constexpr double c = 299792458.;
+  constexpr double c = 299792458.; // m/s
   constexpr double pi = 3.141592653589793238462643;
-  constexpr double earth_radius = 6371315.;
+  constexpr double earth_radius = 6371315.; // m
 
   // other units
   constexpr double proton_mass_gev = 0.93827208816;
