0e081fa56de8837552398bc6520d9f4998a4e9ac
Luan Arbeletche <luan.arbeletche@gmail.com>
2022-07-22 18:43:47 -0300

    define position and time types

--- include/units/units.h
+++ include/units/units.h
@@ -2,6 +2,7 @@
 #define _units_units_h
 
 #include <units/units_impl.h>
+#include <util/vector.h>
 
 namespace units {
   // * define surface density as grams per square centimiter (same unit as atmospheric depth)
@@ -10,13 +11,17 @@ namespace units {
     gram_per_square_cm,
     grams_per_square_cm,
     gcm,
-    compound_unit<mass::grams, inverse<units::squared<length::cm>>>
+    compound_unit<mass::grams, inverse<squared<length::cm>>>
   )
 
-  // * physical quantities using units
-  using height_t = units::length::meter_t;
-  using depth_t = units::surface_density::gram_per_square_cm_t;
-  using density_t = units::density::kilograms_per_liter_t;
+  // * scalar quantities
+  using density_t = density::kilograms_per_liter_t;
+  using depth_t = surface_density::gram_per_square_cm_t;
+  using height_t = length::meter_t;
+  using time_t = time::second_t;
+
+  // * vector quantities
+  using position_t = util::vector<length::meter_t>;
 }
 
 using namespace units::literals;
