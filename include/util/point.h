c2512653b4430df7eaae9dbbcd3b15c128fb40bd
Luan Arbeletche <luan.arbeletche@gmail.com>
2022-07-25 14:56:06 -0300

    refactor operations

--- include/util/point.h
+++ include/util/point.h
@@ -35,6 +35,14 @@ namespace util {
       m_frame(frame)
     {}
 
+    point_t(
+      coordinates_t<T>&& coordinates,
+      const frame_ptr frame
+    ) :
+      coordinates_t<T>(coordinates),
+      m_frame(frame)
+    {}
+
     // * construct a point with given coordinates and frame
     point_t(
       const T& x,
@@ -57,15 +65,18 @@ namespace util {
     )
     {
       if (frame != get_frame()) {
-        // rotate to the standard frame
-        *this = get_frame()->from() * (*this);
-        // move origin to the new frame
-        this->x() += get_frame()->origin()[0] - frame->origin()[0];
-        this->y() += get_frame()->origin()[1] - frame->origin()[1];
-        this->z() += get_frame()->origin()[2] - frame->origin()[2];
-        // rotate to the new frame
-        *this = frame->to() * (*this);
-        // set the frame
+        // copy coordinates of this point but rotated to coincide with the std frame
+        coordinates_t<T> new_coordinates = get_frame()->from() * (*this);
+        // move origin of the coordinates to the new frame
+        new_coordinates.x() += get_frame()->origin()[0] - frame->origin()[0];
+        new_coordinates.y() += get_frame()->origin()[1] - frame->origin()[1];
+        new_coordinates.z() += get_frame()->origin()[2] - frame->origin()[2];
+        // rotate coordinates to the new frame
+        new_coordinates = frame->to() * new_coordinates;
+        // set new frame and coordinates of this point
+        this->x() = new_coordinates.x();
+        this->y() = new_coordinates.y();
+        this->z() = new_coordinates.z();
         m_frame = frame;
       }
       return *this;
@@ -84,68 +95,90 @@ namespace util {
     // - Operations
 
     // * distance to another point
-    template <class U>
-    decltype(T{} - U{}) distance(point_t<U> p) const
+    template <class U, class R = decltype(T{} - U{})>
+    R
+    distance(
+      const point_t<U>& p
+    ) const
     {
-      p.on_frame(get_frame());
-      return std::hypot(this->x()-p.x(), this->y()-p.y(), this->z()-p.z());
+      const auto other = p.on_frame(get_frame());
+      return coordinates_t<R>(
+        this->x() - other.x(),
+        this->y() - other.y(),
+        this->z() - other.z()
+      ).get_r();
     }
 
     // * point-point subtraction produces a vector
-    template <class U>
-    vector_t<decltype(T{}-U{})>
+    template <class U, class R = decltype(T{} - U{})>
+    vector_t<R>
     operator-(
-      point_t<U> p
+      const point_t<U>& p
     ) const
     {
-      p.set_frame(get_frame());
-      return {this->x() - p.x(), this->y() - p.y(), this->z() - p.z(), get_frame()};
+      const auto other = p.on_frame(get_frame());
+      return {
+        this->x() - other.x(),
+        this->y() - other.y(),
+        this->z() - other.z(),
+        get_frame()
+      };
     }
 
     // * point-vector sum/subtraction produces a point
-    template <class U>
-    point_t<decltype(T{}+U{})>
+    template <class U, class R = decltype(T{} + U{})>
+    point_t<R>
     operator+(
-      vector_t<U> v
+      const vector_t<U>& v
     ) const
     {
-      v.set_frame(get_frame());
-      return {this->x() + v[0], this->y() + v[1], this->z() + v[2], get_frame()};
+      const auto other = v.on_frame(get_frame());
+      return {
+        this->x() + other.x(),
+        this->y() + other.y(),
+        this->z() + other.z(),
+        get_frame()
+      };
+    }
+
+    template <class U, class R = decltype(T{} - U{})>
+    point_t<R>
+    operator-(
+      const vector_t<U>& v
+    ) const
+    {
+      const auto other = v.on_frame(get_frame());
+      return {
+        this->x() - other.x(),
+        this->y() - other.y(),
+        this->z() - other.z(),
+        get_frame()
+      };
     }
 
     template <class U>
     point_t<T>&
     operator+=(
-      vector_t<U> v
+      const vector_t<U>& v
     )
     {
-      v.set_frame(get_frame());
-      this->x() += v[0];
-      this->y() += v[1];
-      this->z() += v[2];
+      const auto other = v.on_frame(get_frame());
+      this->x() += other.x();
+      this->y() += other.y();
+      this->z() += other.z();
       return *this;
-    }
-
-    template <class U>
-    point_t<decltype(T{}-U{})>
-    operator-(
-      vector_t<U> v
-    ) const
-    {
-      v.set_frame(get_frame());
-      return {this->x() - v[0], this->y() - v[1], this->z() - v[2], get_frame()};
     }
 
     template <class U>
     point_t<T>&
     operator-=(
-      vector_t<U> v
+      const vector_t<U>& v
     )
     {
-      v.set_frame(get_frame());
-      this->x() -= v[0];
-      this->y() -= v[1];
-      this->z() -= v[2];
+      const auto other = v.on_frame(get_frame());
+      this->x() -= other.x();
+      this->y() -= other.y();
+      this->z() -= other.z();
       return *this;
     }
 
