219ce56f0fb394f9ad51b402afe2a6aed714eb07
Luan Arbeletche <luan.arbeletche@gmail.com>
2022-09-06 17:09:26 -0300

    move romberg integral to math.h

--- include/util/math.h
+++ include/util/math.h
@@ -58,6 +58,44 @@ namespace util::math {
     return ninflec;
   }
 
+	template<class Function>
+	double
+	romberg_integral (
+    const double a,
+    const double b,
+    const double eps, 
+    Function function
+  ) {
+		constexpr int max_it = 30;
+		
+		std::array<double, max_it> v = {0.0};
+		
+		double step = b-a;
+		v[0] = 0.5*step*(function(a) + function(b));
+		
+		double value_before = v[0];
+		int n_steps = 1;
+		
+		for (int k = 1; k < max_it; k++) {
+			n_steps *= 2;
+			step /= 2.0;
+			
+			for (int i = 1; i < n_steps; i+=2)
+				v[k] += function(a + i*step);
+			v[k] = v[k]*step + 0.5*v[k-1];
+			
+			for (int j = k-1; j >= 0; j--)
+				v[j] = v[j+1] + (v[j+1] - v[j]) / (std::pow(4,k-j) - 1.0);
+			
+			if (std::fabs(value_before - v[0]) < eps)
+				return v[0];
+			else
+				value_before = v[0];
+		}
+		
+		return -1;
+	}
+
 } // namespace util::math
 
 #endif
