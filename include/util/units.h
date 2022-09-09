c7e5f5c37b4b1677e316a0308897a8fb1033458e
Luan Arbeletche <luan.arbeletche@gmail.com>
2022-09-09 15:43:50 -0300

    define the electron volt

--- include/util/units.h
+++ include/util/units.h
@@ -1,8 +1,9 @@
 #ifndef _util_units_h
 #define _util_units_h
 
-#include <units/units.h>
 #include <units/angular.h>
+#include <units/framework.h>
+#include <units/units.h>
 
 namespace units {
   // * new units 
@@ -16,6 +17,10 @@ namespace units {
   using grams_per_cubed_centimeter = make_unit<gram, power<centimeter, -3>>;
   units_set_literal(grams_per_cubed_centimeter, gcm3);
   units_set_quantity_alias(grams_per_cubed_centimeter);
+
+  // energy in electron volt
+  units_add_derived_unit(electron_volt, eV, make_unit<joule, yocto, ratio<1, 10'000>>);
+  units_set_prefixes(electron_volt, eV, kilo, mega, giga, tera, peta, exa, zetta);
 
   // * aliases for scalar quantities
 
