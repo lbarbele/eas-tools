30170c47028b685b30ad19aaaca3f45f8f89512b
Luan Arbeletche <luan.arbeletche@gmail.com>
2022-09-12 20:24:46 -0300

    util::math is header only

--- include/util/math.h
+++ include/util/math.h
@@ -11,7 +11,7 @@ namespace util::math {
 
   using namespace units::math;
 
-  double
+  inline double
   gaisser_hillas(
     const double x,
     const double nmax,
@@ -20,24 +20,48 @@ namespace util::math {
     const double p1,
     const double p2 = 0,
     const double p3 = 0
-  );
+  )
+  {
+    if (x <= x0) {
+      return 0;
+    }
 
-  double
+    const double l = p1 + x*(p2 + x*p3);
+    const double a = (xmax - x0) / l;
+    const double r = (x - x0) / (xmax - x0);
+    return nmax * std::exp(a * (1 - r + std::log(r)));
+  }
+
+  inline double
   gaisser_hillas_ecal(
     const double x,
     const double ecal,
     const double x0,
     const double xmax,
     const double lambda
-  );
+  )
+  {
+    if (x <= x0) {
+      return 0;
+    }
+    
+    const double z = (x - x0) / lambda;
+    const double a = (xmax - x0) / lambda;
+    return (ecal/lambda) * std::exp(-std::lgamma(a+1) - z + a*std::log(z));
+  }
 
-  double usp_ecal(
+  inline double usp_ecal(
     const double x,
     const double ecal,
     const double xmax,
     const double l,
     const double r
-  );
+  )
+  {
+    const double invr2 = 1.0/(r*r);
+    const double z = (1.0/r + (x-xmax)/l) / r;
+    return z<=0? 0 : ecal*std::pow(z,invr2)*std::exp(-z)/(l*r*std::tgamma(1+invr2));
+  }
   
   template <typename T>
   unsigned int
