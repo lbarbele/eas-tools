3d850e42abade3a0b24eb57f7c3355ac4832f949
Luan Arbeletche <luan.arbeletche@gmail.com>
2022-09-20 20:22:51 -0300

    make binaryfile header-only

--- include/corsika/binaryfile.h
+++ include/corsika/binaryfile.h
@@ -5,9 +5,10 @@
 #include <corsika/binarystream.h>
 #include <corsika/subblock.h>
 
-#include <vector>
+#include <iostream>
 #include <memory>
 #include <string>
+#include <vector>
 
 namespace corsika {
 
@@ -19,7 +20,32 @@ namespace corsika {
     std::vector<shower> m_showers;
 
   public:
-    binaryfile(const std::string& fname);
+    binaryfile(const std::string& fname)
+    : m_stream(std::make_shared<binarystream>(fname, std::ios::in))
+    {
+      // consume the stream until the event end is found
+      for (auto it = m_stream->begin(); it != m_stream->end(); ++it) {
+        switch (it->get_type())
+        {
+        case subblock::type::run_header:
+          m_header = *it;
+          continue;
+        case subblock::type::run_end:
+          m_trailer = *it;
+          return;
+        case subblock::type::event_header:
+          m_showers.emplace_back(m_stream, it);
+          continue;
+        default:
+          std::cerr << "corsika::binaryfile::binaryfile(): unexpected block " << it->get_title() << std::endl;
+          throw;
+        }
+      }
+
+      // if we get here, the event trailer was not found
+      std::cerr << "corsika::binaryfile::binaryfile(): could not found the event trailer block!" << std::endl;
+      throw;
+    }
 
     const subblock& get_header() const
     {return m_header;}
